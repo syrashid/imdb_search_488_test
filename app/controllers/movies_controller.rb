94825d6510437c5a479974664d635b939bf93c89
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-25 17:59:42 +0100

    FEAT: Modified search functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,10 +2,10 @@ class MoviesController < ApplicationController
   def index
     if params[:query].present?
       sql_query = " \
-        movies.title ILIKE :query \
-        OR movies.syllabus ILIKE :query \
-        OR directors.first_name ILIKE :query \
-        OR directors.last_name ILIKE :query \
+        movies.title @@ :query \
+        OR movies.syllabus @@ :query \
+        OR directors.first_name @@ :query \
+        OR directors.last_name @@ :query \
       "
       @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
     else
