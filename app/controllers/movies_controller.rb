88eddf8e67c02697b86ec37a6a5b5e9608e7d23b
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-25 18:07:04 +0100

    FEAT: Added pg multisearch

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
   def index
     if params[:query].present?
-      @movies = Movie.search_by_title_and_syllabus(params[:query])
+      @movies = Movie.global_search(params[:query])
     else
       @movies = Movie.all
     end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,9 +2,5 @@ class Movie < ApplicationRecord
   belongs_to :director
 
   include PgSearch::Model
-  pg_search_scope :search_by_title_and_syllabus,
-    against: [ :title, :syllabus ],
-    using: {
-      tsearch: { prefix: true } # <-- now `superman batm` will return something!
-    }
+  multisearchable against: [:title, :syllabus]
 end
